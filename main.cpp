58fd2ef7c1c9271967a6239f356b151c675facbf
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2024-05-11 20:49:26 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <pqxx/pqxx>
+#include "Connection.h"
+
+using namespace std;
+using namespace pqxx;
+
+int main() {
+    string dbname = "rallye";
+    string user = "postgres";
+    string password = "kaloina1501";
+    string hostaddr = "localhost";
+    string port = "5432";
+
+    Connection conn(dbname,user,password,hostaddr,port);
+
+    connection *con = conn.getConnection();
+
+    return 0;
+}
